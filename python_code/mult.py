3c2a7e538d98c43374c71d6e25e9fffd5d042679
James <kjames@lfny-ltm0774.home>
2016-12-28 22:53:01 -0500

    mult func print

---
+++ python_code/mult.py
@@ -0,0 +1,13 @@
+def mult(a, b):
+	answer = a * b
+	prompt = "The answer of {} and {} is .".format(a, b, mult)
+	print(answer)
+
+def main ():
+	a = int(input("what is the first number?"))
+	b = int(input("what is the second number?"))
+	(a * b)
+	mult(a, b)
+
+main()
+
