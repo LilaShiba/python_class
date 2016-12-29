b7019b93afb84b2c3a1513144fea9cced58465dc
James <kjames@lfny-ltm0774.home>
2016-12-28 22:55:45 -0500

    mult print format

--- python_code/mult.py
+++ python_code/mult.py
@@ -1,7 +1,9 @@
+# http://anh.cs.luc.edu/python/hands-on/3.1/handsonHtml/functions.html
+
 def mult(a, b):
 	answer = a * b
-	prompt = "The answer of {} and {} is .".format(a, b, mult)
-	print(answer)
+	prompt = "The sum of {} and {} is {}.".format(a, b, answer)
+	print(prompt)
 
 def main ():
 	a = int(input("what is the first number?"))
